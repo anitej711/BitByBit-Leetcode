fd4f193b2d06c0bdbb4081688ea8a760bfea34fc
anitej711 <ani.teju123@gmail.com>
2025-11-01 00:34:54 +0530

    common factors logic in 2427.py
    Refactor variable assignment and condition formatting for clarity.

--- 2427.py
+++ 2427.py
@@ -7,15 +7,15 @@ class Solution(object):
         """
         greater=0
         smaller=0
-        if a > b:
-            greater = a
-            smaller = b
+        if a>b:
+            greater=a
+            smaller=b
         else:
-            greater = b
-            smaller = a
+            greater=b
+            smaller=a
 
         c=0
         for i in range(1,greater+1):
-            if greater % i==0 and smaller % i ==0:
+            if greater%i==0 and smaller%i==0:
                 c+=1
         return c
