1df721a7f825e885e96e054b62ed1332c5b54fb2
anitej711 <ani.teju123@gmail.com>
2025-11-03 16:59:17 +0530

    Add rotate method to rotate matrix 90 degrees
    Implement matrix rotation in-place by transposing and reversing.

---
+++ 48.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        #find trp and then reverse
+
+        n=len(matrix)
+        for i in range(n):
+            for j in range(i+1,n):
+                matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]  #normal trp
+
+        for i in range(n):
+            matrix[i].reverse()
